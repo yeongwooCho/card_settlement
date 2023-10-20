fae59d1d766a0ccc1fcb55ade8adc1e58dfefd2d
yeongwoo <jyy0223@naver.com>
2023-10-20 20:43:43 +0900

    [add] 개인정보 수정 페이지 text에서 textformfield 로 변경하는 코드 추가

--- lib/my_page/view/settings_screen.dart
+++ lib/my_page/view/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:card_settlement/common/component/custom_text_form_field.dart';
 import 'package:card_settlement/common/const/text_style.dart';
 import 'package:card_settlement/common/layout/default_appbar.dart';
 import 'package:card_settlement/common/layout/default_layout.dart';
@@ -5,51 +6,72 @@ import 'package:flutter/material.dart';
 
 import '../../common/const/colors.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isFirstEditSelected = false;
+  bool isSecondEditSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       appbar: const DefaultAppBar(title: '개인 정보 관리'),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
-        child: Column(
-          children: [
-            RenderContainer(
-              title: '사업자 정보',
-              fixButtonWidget: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  '정보 수정',
-                  style: MyTextStyle.bodyMedium,
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
+          child: Column(
+            children: [
+              RenderContainer(
+                title: '사업자 정보',
+                fixButtonWidget: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isFirstEditSelected = !isFirstEditSelected;
+                    });
+                  },
+                  child: Text(
+                    isFirstEditSelected ? '수정 완료' : '정보 수정',
+                    style: MyTextStyle.bodyMedium,
+                  ),
                 ),
+                data: [
+                  ['상호명', '쿠팡(주)'],
+                  ['사업자 번호', '120-88-00767'],
+                  ['휴대폰 번호', '010-1234-1234'],
+                  ['사업장 주소', '서울시 송파구 송파대로 570번길'],
+                ],
+                isEditSelected: isFirstEditSelected,
               ),
-              data: [
-                ['상호명', '쿠팡(주)'],
-                ['사업자 번호', '120-88-00767'],
-                ['휴대폰 번호', '010-1234-1234'],
-                ['사업장 주소', '서울시 송파구 송파대로 570번길'],
-              ],
-            ),
-            const SizedBox(height: 32.0),
-            RenderContainer(
-              title: '정산 정보',
-              fixButtonWidget: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  '정보 수정',
-                  style: MyTextStyle.bodyMedium,
+              const SizedBox(height: 32.0),
+              RenderContainer(
+                title: '정산 정보',
+                fixButtonWidget: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isSecondEditSelected = !isSecondEditSelected;
+                    });
+                  },
+                  child: Text(
+                    isSecondEditSelected ? '수정 완료' : '정보 수정',
+                    style: MyTextStyle.bodyMedium,
+                  ),
                 ),
+                data: [
+                  ['은행', '농협은행'],
+                  ['계좌번호', '702022-123-234'],
+                  ['입금주명', '홍길동'],
+                  ['결제 날짜', '15일'],
+                ],
+                isEditSelected: isSecondEditSelected,
               ),
-              data: [
-                ['은행', '농협은행'],
-                ['계좌번호', '702022-123-234'],
-                ['입금주명', '홍길동'],
-                ['결제 날짜', '15일'],
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -60,12 +82,14 @@ class RenderContainer extends StatelessWidget {
   final String title;
   final Widget fixButtonWidget;
   final List<List<String>> data;
+  final bool isEditSelected;
 
   const RenderContainer({
     super.key,
     required this.title,
     required this.fixButtonWidget,
     required this.data,
+    required this.isEditSelected,
   });
 
   @override
@@ -93,9 +117,10 @@ class RenderContainer extends StatelessWidget {
             child: Column(
               children: List.generate(
                 data.length,
-                (index) => _renderRow(
+                    (index) => _renderRow(
                   title: data[index].first,
                   description: data[index].last,
+                  isEditSelected: isEditSelected,
                 ),
               ),
             ),
@@ -108,11 +133,12 @@ class RenderContainer extends StatelessWidget {
   Widget _renderRow({
     required String title,
     required String description,
+    required bool isEditSelected,
   }) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           SizedBox(
             width: 120.0,
@@ -122,10 +148,18 @@ class RenderContainer extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Text(
-              description,
-              style: MyTextStyle.bodyRegular,
-            ),
+            child: isEditSelected
+                ? CustomTextFormField(
+                    onChanged: (String? value) {},
+                    onSaved: (String? value) {},
+                    validator: (String? value) {
+                      return null;
+                    },
+                  )
+                : Text(
+                    description,
+                    style: MyTextStyle.bodyRegular,
+                  ),
           ),
         ],
       ),
